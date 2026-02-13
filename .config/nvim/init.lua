f38c9246ea3b6c0257d1eec960ecb0e2f3afa76d
Ignacy Pękała <ignacy@pekala.eu>
2026-02-13 21:48:35 +0100

    Tweaks config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,21 @@
+
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.numberwidth = 4
-
-vim.opt.colorcolumn = "80"
 
 vim.opt.wrap = false
 
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = 'both'
 vim.opt.scrolloff = 999
--- Normal cursor but with a beam in the command line
-vim.opt.guicursor = "n-v-sm:block,i-c-ci-ve:ver25-blinkon500,r-cr-o:hor20,t:block-blinkon500-blinkoff500-TermCursor"
+-- Normal cursor but with a beam when typing
+vim.opt.guicursor = "n-v-sm:block,i-c-ci-ve:ver25-blinkon500,"..
+    "r-cr-o:hor20,t:block-blinkon500-blinkoff500-TermCursor"
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
--- Use 4 spaces as tab, 4 if not at the beginning of a line
--- I think treesitter overwrites it on supported filetypes
+vim.opt.colorcolumn = "80"
+
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
@@ -27,29 +29,28 @@ vim.opt.fileformat = 'unix'
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
--- TODO: Change the color to something defined by colorschemes
--- vim.cmd('highlight SignColumn guifg=red')
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Alt+hjkl in command mode
--- Currently not used as I have the same overwrites in alacritty config
+-- alt+hjkl in command mode
+-- Currently not used as i have the same overwrites in alacritty config
 -- Leaving this just in case I switch terminal emulators
-vim.keymap.set({"c"}, "<M-h>", "<Left>")
-vim.keymap.set({"c"}, "<M-H>", "<S-Left>")
-vim.keymap.set({"c"}, "<M-l>", "<Right>")
-vim.keymap.set({"c"}, "<M-L>", "<S-Right>")
-vim.keymap.set({"c"}, "<M-j>", "<Down>")
-vim.keymap.set({"c"}, "<M-k>", "<Up>")
+vim.keymap.set({"c"}, "<m-h>", "<left>")
+vim.keymap.set({"c"}, "<m-h>", "<s-left>")
+vim.keymap.set({"c"}, "<m-l>", "<right>")
+vim.keymap.set({"c"}, "<m-l>", "<s-right>")
+vim.keymap.set({"c"}, "<m-j>", "<down>")
+vim.keymap.set({"c"}, "<m-k>", "<up>")
 
 -- Move chunks of text in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- github.com/ThePrimeagen/neovimrc
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
 
+-- Appearance
 vim.opt.termguicolors = false
+vim.opt.winborder = "none"
 vim.cmd.colorscheme('custom')
-vim.opt.fillchars = { vert = " ", horiz = " " , eob = " " }
 
 require('config.clipboard')
 require('config.lazy')

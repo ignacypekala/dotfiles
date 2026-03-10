efc18003b4e4f7525c2e3894701a5ee2eabb7bd6
Ignacy Pękała <ignacy@pekala.eu>
2026-03-10 21:16:24 +0100

    Adjust clipboard keymap

--- .config/nvim/lua/config/clipboard.lua
+++ .config/nvim/lua/config/clipboard.lua
@@ -2,10 +2,12 @@
 -- wezterm only supports writing so paste with ctrl+shift+v
 vim.g.clipboard = 'osc52'
 
--- prefix clipboard operation with leader to copy to system clipboard
+-- prefix clipboard operation with leader to interact with the system clipboard
 vim.keymap.set({'v', 'n'}, '<leader>y', '"+y', {
     desc = "Yank (system clipboard)",
     noremap = true
 })
-vim.keymap.set('v', '<leader>x', '"+x', { desc = "Cut character (system clipboard)" })
-vim.keymap.set({ 'v', 'n' }, '<leader>p', '"+p', { desc = "Paste after cursor (system clipboard)" })
+
+vim.keymap.set({ 'v', 'n' }, '<leader>p', '"+p', {
+    desc = "Paste after cursor (system clipboard)"
+})

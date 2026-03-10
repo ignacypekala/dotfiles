56cce0c3e651a37702ed342623fc5027f4e02b3e
Ignacy Pękała <ignacy@pekala.eu>
2026-03-10 21:00:25 +0100

    Change wsl clipboard to osc 52

--- .config/nvim/lua/config/clipboard.lua
+++ .config/nvim/lua/config/clipboard.lua
@@ -1,28 +1,11 @@
-local copy = '/mnt/c/Windows/System32/clip.exe'
-local paste = '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
-
--- Clipboard setup for WSL
-if (vim.fn.getenv("WSL_DISTRO_NAME") ~= vim.NIL) then
-    vim.g.clipboard = {
-        name = 'wsl',
-        copy = {
-            ['+'] = copy,
-            ['*'] = copy,
-        },
-        paste = {
-            ['+'] = paste,
-            ['*'] = paste,
-        },
-        cache_enabled = 0,
-    }
-end
+-- Use OSC 52 for the clipboard
+-- wezterm only supports writing so paste with ctrl+shift+v
+vim.g.clipboard = 'osc52'
 
 -- prefix clipboard operation with leader to copy to system clipboard
-vim.keymap.set('v', '<leader>y', '"+y', { desc = "Yank (system clipboard)" })
+vim.keymap.set({'v', 'n'}, '<leader>y', '"+y', {
+    desc = "Yank (system clipboard)",
+    noremap = true
+})
 vim.keymap.set('v', '<leader>x', '"+x', { desc = "Cut character (system clipboard)" })
 vim.keymap.set({ 'v', 'n' }, '<leader>p', '"+p', { desc = "Paste after cursor (system clipboard)" })
-
--- vim.keymap.set('n', '<leader>yy', '"+yy', { desc = "Yank line (system clipboard)" })
--- vim.keymap.set({ 'v', 'n' }, '<leader>P', '"+P', { desc = "Paste before cursor (system clipboard)" })
-
-

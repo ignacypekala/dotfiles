bb592876709fd33daa199d9e93f5f2482a57545c
Ignacy Pękała <ignacy@pekala.eu>
2026-03-20 14:08:35 +0100

    Remove space

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,7 +8,7 @@ local setup_lua_ls = function()
                 local path = client.workspace_folders[1].name
                 if
                     path ~= vim.fn.stdpath('config')
-                    and (vim.uv.fs_stat(path .. '/.luarc.json') 
+                    and (vim.uv.fs_stat(path .. '/.luarc.json')
                     or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
                 then return end
             end

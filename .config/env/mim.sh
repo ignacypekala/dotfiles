df7f210b37c61a5f403d998e3211b110873bf6fa
Ignacy Pękała <ignacy@pekala.eu>
2026-03-21 20:27:42 +0100

    Start ssh-agent from env/mim.sh

--- .config/env/mim.sh
+++ .config/env/mim.sh
@@ -3,3 +3,9 @@ export WORK_DIRS=(
     "$HOME/laby"
 )
 add_path ~/.local/bin
+
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+if [ ! -S "$SSH_AUTH_SOCK" ]; then
+    eval "$(ssh-agent -a "$SSH_AUTH_SOCK")" > /dev/null
+fi
+

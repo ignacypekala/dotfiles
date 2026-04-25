f93dadcc0f4b254a23579493baaea7c3239ff9fd
Ignacy Pękała <ignacy@pekala.eu>
2026-04-25 14:45:35 +0200

    Keep more path after trimming

--- .config/scripts/path-trimmer.sh
+++ .config/scripts/path-trimmer.sh
@@ -14,17 +14,18 @@ done
 
 num_segments=${#segments[@]}
 
-if [ "$num_segments" -le 3 ]; then
-    # If 3 or fewer, just print the path
+if [ "$num_segments" -le 4 ]; then
+    # If 4 or fewer, just print the path
     echo "$input_path"
 else
-    # If more than 3, format as: first/.../n-2/n-1
+    # If more than 4, format as: first/.../n-3/n-2/n-1
     first="${segments[0]}"
-    penultimate="${segments[$num_segments-2]}"
-    last="${segments[$num_segments-1]}"
+    n_3="${segments[$num_segments-3]}"
+    n_2="${segments[$num_segments-2]}"
+    n_1="${segments[$num_segments-1]}"
     
     # Handle absolute vs relative start
     [[ "$input_path" == /* ]] && prefix="/" || prefix=""
     
-    echo "${prefix}${first}/.../${penultimate}/${last}"
+    echo "${prefix}${first}/.../${n_3}/${n_2}/${n_1}"
 fi

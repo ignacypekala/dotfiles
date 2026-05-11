9ca2d14618699479d4ccef3caa68c78da79b8e9f
Ignacy Pękała <ignacy@pekala.eu>
2026-05-11 14:47:18 +0200

    Move the jdtls setup to jdtls.java

--- .config/nvim/lua/plugins/jdtls.lua
+++ .config/nvim/lua/plugins/jdtls.lua
@@ -2,9 +2,31 @@ local codeberg = require('utils.pack_providers').codeberg
 
 vim.pack.add({ codeberg .. "mfussenegger/nvim-jdtls" })
 
+local bundles = {}
+local java_debug = vim.env.JAVA_DEBUG
+if java_debug ~= 0 then
+    bundles = {
+        vim.fn.glob(java_debug, true)
+    }
+end
+
+local config = {
+    name = "jdtls",
+    cmd = { "jdtls" },
+    root_dir = vim.fs.root(0, {'gradlew', '.git', 'mvnw'}),
+    settings = {
+        java = {
+
+        }
+    },
+    init_options = {
+        bundles = bundles,
+    }
+}
+
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "java",
     callback = function()
-        require('debuggers.java').start_or_attach()
+        require('jdtls').start_or_attach(config)
     end
 })

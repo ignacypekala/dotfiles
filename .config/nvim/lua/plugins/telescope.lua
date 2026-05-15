36399e601d80f97420746c1091a1077343123684
Ignacy Pękała <ignacy@pekala.eu>
2026-05-15 17:41:17 +0200

    Remove --cached from telescope modified

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -127,7 +127,6 @@ vim.keymap.set('n', '<leader>v',
                     "core.quotepath=false",
                     "ls-files",
                     "--exclude-standard",
-                    "--cached",
                     "--modified",
                 }
             })

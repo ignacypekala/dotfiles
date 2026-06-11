87be8226108e1dede55ee1a9b315479e79b55579
Ignacy Pękała <ignacy@pekala.eu>
2026-06-12 00:09:24 +0200

    Trim tmux path relative to screen size

--- .config/tmux/update-path.sh
+++ .config/tmux/update-path.sh
@@ -3,6 +3,10 @@
 raw_path="$1"
 session_id="$2"
 
-display_path=$(tmux-name-desanitize.sh "$raw_path" | path-format.sh | path-trim.sh 50)
+display_path=$(
+    tmux-name-desanitize.sh "$raw_path" \
+    | path-format.sh \
+    | path-trim-relative.sh 0.4 20
+)
 
 tmux set-environment -t "$session_id" session_display_path "$display_path"

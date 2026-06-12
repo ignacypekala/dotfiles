d9fd0d0f8bd96b17209d050467ec2dea80c31d70
Ignacy Pękała <ignacy@pekala.eu>
2026-06-12 16:15:59 +0200

    Ensure tmux session display path is updated on update

--- .config/tmux/update-path.sh
+++ .config/tmux/update-path.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 raw_path="$1"
-session_id="$2"
+session="$2"
 
 display_path=$(
     tmux-name-desanitize.sh "$raw_path" \
@@ -9,4 +9,5 @@ display_path=$(
     | path-trim-relative.sh 0.4 20
 )
 
-tmux set-environment -t "$session_id" session_display_path "$display_path"
+tmux set-environment -t "$session" session_display_path "$display_path"
+tmux refresh-client -S

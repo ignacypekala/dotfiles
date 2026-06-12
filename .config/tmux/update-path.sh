c74d9091d2c59e4c654ce531bdf065463ea5acea
Ignacy Pękała <ignacy@pekala.eu>
2026-06-12 16:53:37 +0200

    Fix tmux path trimming

--- .config/tmux/update-path.sh
+++ .config/tmux/update-path.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+# Reformat tmux session display path for a given client.
+#
+# Usage:
+# ./update-path.sh [raw_path] [session] [client]
 
 raw_path="$1"
 session="$2"
+client="$3"
+
+tmux_width=$(tmux display-message -p -c "$session" "#{client_width}")
+max_length=$(( $tmux_width * 40 / 100 ))
 
 display_path=$(
     tmux-name-desanitize.sh "$raw_path" \
     | path-format.sh \
-    | path-trim-relative.sh 0.4 20
+    | path-trim.sh $max_length
 )
 
 tmux set-environment -t "$session" session_display_path "$display_path"

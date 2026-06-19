bbbd9ffc74ecdd76ab1fb81320feb09bddd791c1
Ignacy Pękała <ignacy@pekala.eu>
2026-06-19 20:10:10 +0200

    Correct lualine yellow

--- .config/nvim/lua/lualine-theme.lua
+++ .config/nvim/lua/lualine-theme.lua
@@ -8,7 +8,7 @@ return {
     insert = { a = { fg = c.ansi.black, bg = c.ansi.green, gui = 'bold' } },
     visual = { a = { fg = c.ansi.black, bg = c.ansi.purple, gui = 'bold' } },
     replace = { a = { fg = c.ansi.black, bg = c.ansi.red, gui = 'bold' } },
-    command = { a = { fg = c.ansi.black, bg = c.ansi.yellow, gui = 'bold' } },
+    command = { a = { fg = c.ansi.black, bg = c.bright.yellow, gui = 'bold' } },
     inactive = {
         a = { fg = c.bright.black, bg = "NONE", gui = 'bold' },
         b = { fg = c.bright.black, bg = "NONE" },

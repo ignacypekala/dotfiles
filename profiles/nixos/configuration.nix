9994547a10b9fc1a120c5f8210d1ac051f485bed
Ignacy Pękała <ignacy@pekala.eu>
2026-07-01 23:28:15 +0200

    Install fonts

--- profiles/nixos/configuration.nix
+++ profiles/nixos/configuration.nix
@@ -1,146 +1,149 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
+    imports =
+        [
+            ./hardware-configuration.nix
+        ];
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "nixos"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "Europe/Warsaw";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "pl_PL.UTF-8";
+        LC_IDENTIFICATION = "pl_PL.UTF-8";
+        LC_MEASUREMENT = "pl_PL.UTF-8";
+        LC_MONETARY = "pl_PL.UTF-8";
+        LC_NAME = "pl_PL.UTF-8";
+        LC_NUMERIC = "pl_PL.UTF-8";
+        LC_PAPER = "pl_PL.UTF-8";
+        LC_TELEPHONE = "pl_PL.UTF-8";
+        LC_TIME = "pl_PL.UTF-8";
+    };
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+    };
+
+    users.users."ignacy" = {
+        isNormalUser = true;
+        description = "ignacy";
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [
+        ];
+    };
+
+    nixpkgs.config.allowUnfree = true;
+
+    environment.systemPackages = with pkgs; [
+        gcc
+        bash
+        git
+        tmux
+        stow
+
+        wl-clipboard
+
+        librewolf
+        wezterm
+        fastfetch
+        quickshell
+        discord
+        gimp
+
+        autotiling
+
+        neovim
+        tree-sitter
+        lua-language-server
+        libclang
+        jdt-language-server
+        kdePackages.qtdeclarative # for qmlls for quickshell
+
+        fortune
+        fzf
+        ripgrep
+
+        tree
+        file
+        zip
+        unzip
+        htop
+
+        libnotify
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Warsaw";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pl_PL.UTF-8";
-    LC_IDENTIFICATION = "pl_PL.UTF-8";
-    LC_MEASUREMENT = "pl_PL.UTF-8";
-    LC_MONETARY = "pl_PL.UTF-8";
-    LC_NAME = "pl_PL.UTF-8";
-    LC_NUMERIC = "pl_PL.UTF-8";
-    LC_PAPER = "pl_PL.UTF-8";
-    LC_TELEPHONE = "pl_PL.UTF-8";
-    LC_TIME = "pl_PL.UTF-8";
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  users.users."ignacy" = {
-    isNormalUser = true;
-    description = "ignacy";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
+    fonts.packages = with pkgs; [
+        noto-fonts
+        fira-sans
+        jetbrains-mono
+        nerd-fonts.symbols-only
     ];
-  };
 
-  nixpkgs.config.allowUnfree = true;
+    services.gnome.gnome-keyring.enable = true;
 
-  environment.systemPackages = with pkgs; [
-	gcc
-	bash
-  	git
-	tmux
-	stow
-    
-	wl-clipboard
+    programs.sway = {
+        enable = true;
+        wrapperFeatures.gtk = true;
+    };
 
-	librewolf
-	wezterm
-    fastfetch
-    quickshell
-    discord
-    gimp
+    programs.steam = {
+        enable = true;
+    };
 
-    autotiling
+    # Enable proprietary NVIDIA drivers
+    services.xserver.videoDrivers = ["nvidia"];
+    hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = false;
+        powerManagement.finegrained = false;
+        open = true;
+        nvidiaSettings = true;
+    };
 
-	neovim
-    tree-sitter
-    lua-language-server
-    libclang
-    jdt-language-server
-    kdePackages.qtdeclarative # for qmlls for quickshell
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
 
-    fortune
-    fzf
-    ripgrep
+    # List services that you want to enable:
 
-    tree
-    file
-    zip
-    unzip
-    htop
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
 
-    libnotify
-  ];
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
-  fonts.packages = with pkgs; [
-    fira-sans
-  ];
-
-  services.gnome.gnome-keyring.enable = true;
-
-  programs.sway = {
- 	enable = true;
- 	wrapperFeatures.gtk = true;
-  };
-
-  programs.steam = {
-    enable = true;
-  };
-
-  # Enable proprietary NVIDIA drivers
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-      modesetting.enable = true;
-      powerManagement.enable = false;
-      powerManagement.finegrained = false;
-      open = true;
-      nvidiaSettings = true;
-  };
-
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = {
-#   enable = true;
-#   enableSSHSupport = true;
-# };
-
-# List services that you want to enable:
-
-# Enable the OpenSSH daemon.
-# services.openssh.enable = true;
-
-# Open ports in the firewall.
-# networking.firewall.allowedTCPPorts = [ ... ];
-# networking.firewall.allowedUDPPorts = [ ... ];
-# Or disable the firewall altogether.
-# networking.firewall.enable = false;
-
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.05"; # Did you read the comment?
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "26.05"; # Did you read the comment?
 
 }

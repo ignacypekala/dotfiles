0a002dec8293c48ec5f08b851b9c47a39bffc45b
Ignacy Pękała <ignacy@pekala.eu>
2026-07-02 19:48:42 +0200

    Auto float bitwarden

--- profiles/hyprland/.config/hypr/hyprland.lua
+++ profiles/hyprland/.config/hypr/hyprland.lua
@@ -264,6 +264,15 @@ hl.window_rule({
     suppress_event = "maximize",
 })
 
+-- Make bitwarden prompts floating (float canot be set based on a title change
+-- with window rules).
+hl.on("window.title", function(w)
+    if w ~= nil and w.title == "Extension: (Bitwarden Password Manager) - Bitwarden — LibreWolf" then
+        hl.dispatch(hl.dsp.window.float({ action = "set" }))
+    end
+end
+)
+
 -- Fix some dragging issues with XWayland
 hl.window_rule({
     name  = "fix-xwayland-drags",

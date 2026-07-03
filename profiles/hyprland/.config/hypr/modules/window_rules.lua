d99ff1bf44f39758087d71a95123c3fc346d86b2
Ignacy Pękała <ignacy@pekala.eu>
2026-07-03 20:54:59 +0200

    Games workspace

--- profiles/hyprland/.config/hypr/modules/window_rules.lua
+++ profiles/hyprland/.config/hypr/modules/window_rules.lua
@@ -32,3 +32,22 @@ hl.window_rule({
     move  = "20 monitor_h-120",
     float = true,
 })
+
+hl.window_rule({
+    name  = "classify-steam-apps-as-games",
+    match = { initial_class = "steam_app_\\d+" },
+    content = "game"
+
+})
+
+hl.workspace_rule({
+    workspace = "name:game",
+    monitor = "DP-2"
+})
+
+hl.window_rule({
+    name = "move-games-to-games-workspace",
+    match = { content = "game" },
+    workspace = "name:game",
+    fullscreen = true,
+})

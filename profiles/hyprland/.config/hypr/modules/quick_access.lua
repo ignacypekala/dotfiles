8280f9ea88000227a5c60add5286d0d2db9bdc40
Ignacy Pękała <ignacy@pekala.eu>
2026-07-03 14:35:56 +0200

    Rename program_workspaces to quick_access and change to prefix submap

---
+++ profiles/hyprland/.config/hypr/modules/quick_access.lua
@@ -0,0 +1,44 @@
+local mod = require('utils.mod')
+local run_cmd = require('utils.run_cmd')
+
+local common_programs = {
+    {
+        class = "discord",
+        workspace = "discord",
+        app = "discord",
+        key = "D"
+    },
+    {
+        class = "steam",
+        workspace = "steam",
+        app = "steam",
+        key = "G" -- Games
+    },
+    {
+        class = "librewolf",
+        workspace = "browser",
+        app = "librewolf",
+        key = "W" -- Web
+    }
+}
+
+hl.define_submap("launch", "reset", function()
+    for _, program in ipairs(common_programs) do
+        local handler = function()
+            local window = hl.get_window("class:" .. program.class)
+            if window ~= nil then
+                hl.dispatch(hl.dsp.focus({
+                    window = window
+                }))
+            else
+                hl.dispatch(hl.dsp.exec_cmd(run_cmd(program.app)))
+            end
+        end
+        hl.bind(program.key, handler)
+        hl.bind(mod(program.key), handler)
+        hl.bind("SHIFT + " .. program.key, handler)
+    end
+    hl.bind("catchall", function() end)
+end)
+
+hl.bind(mod("E"), hl.dsp.submap("launch"))

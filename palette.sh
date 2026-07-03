af95adbbced4492984f3d952aa3a81b3498c37e0
Ignacy Pękała <ignacy@pekala.eu>
2026-07-03 16:52:15 +0200

    Add palette.sh

---
+++ palette.sh
@@ -0,0 +1,74 @@
+# Source colors
+
+neutral_900="#050505"
+neutral_800="#121212"
+neutral_700="#262626"
+neutral_600="#404040"
+neutral_500="#5e5e5e"
+neutral_400="#808080"
+neutral_300="#a6a6a6"
+neutral_200="#cccccc"
+neutral_100="#ffffff"
+
+# base_black="#232323"
+base_red="#d44d4f"
+base_green="#67a376"
+base_yellow="#3c372c"
+base_blue="#78a7f0"
+base_purple="#b575e9"
+base_cyan="#4eccf2"
+# base_gray="#b3b3b3"
+
+# base_bright_black="#5e5d5d"
+base_bright_red="#ed7375"
+base_bright_green="#9ae3ac"
+base_bright_yellow="#f9f1a5"
+base_bright_blue="#8bc4f0"
+base_bright_purple="#c59be7"
+base_bright_cyan="#9fd3ed"
+# base_bright_gray="#d6d6d6"
+
+base_tint_black="#121212"
+base_tint_red="#4b2526"
+base_tint_green="#233726"
+base_tint_yellow="#1f1e1c"
+base_tint_blue="#28364d"
+base_tint_purple="#493857"
+base_tint_cyan="#32424a"
+base_tint_white="#343638"
+
+# Semantic aliases
+
+ui_bg="$neutral_800"
+ui_bg_deep="$neutral_900"
+ui_bg_elevated="$neutral_700"
+ui_bg_active="$neutral_600"
+
+ui_fg="$neutral_200"
+ui_fg_bold="$neutral_100"
+ui_fg_subtle="$neutral_400"
+ui_fg_muted="$neutral_500"
+
+ui_accent_primary="$base_green"
+ui_accent_secondary="$base_magenta"
+
+ui_border="$neutral_600"
+ui_border_subtle="$neutral_700"
+ui_border_active="$ui_accent_primary"
+
+state_info="$base_blue"
+state_success="$base_green"
+state_warning="$base_yellow"
+state_error="$base_red"
+
+diff_add_line_bg="$base_dim_green"
+diff_add_bg="$base_green"
+diff_add_fg="$neutral_200"
+
+diff_remove_line_bg="$base_dim_red"
+diff_remove_bg="$base_red"
+diff_remove_fg="$neutral_200"
+
+diff_change_line_bg="$base_dim_blue"
+diff_change_bg="$base_blue"
+diff_change_fg="$neutral_200"

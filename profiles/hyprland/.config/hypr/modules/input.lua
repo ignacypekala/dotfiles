b8f1b7c140623835db822131d09e884f07a1d1ab
Ignacy Pękała <ignacy@pekala.eu>
2026-07-06 01:21:31 +0200

    Disable mouse acceleration in game workspace

--- profiles/hyprland/.config/hypr/modules/input.lua
+++ profiles/hyprland/.config/hypr/modules/input.lua
@@ -25,10 +25,29 @@ hl.gesture({
     action = "workspace"
 })
 
-hl.device({
-    name = "sinowealth-wired-gaming-mouse",
-    sensitivity = -0.5,
-})
+local function register_mouse(acceleration)
+    local name = "sinowealth-wired-gaming-mouse"
+    if acceleration then
+        hl.device({
+            name = name,
+            sensitivity = -0.5,
+            accel_profile = ""
+        })
+    else
+        hl.device({
+            name = name,
+            sensitivity = -0.2,
+            accel_profile = "flat"
+        })
+    end
+end
+
+register_mouse(true)
+
+hl.on("workspace.active", function (workspace)
+    local accel = not (workspace ~= nil and workspace.name ~= nil and workspace.name == "game")
+    register_mouse(accel)
+end)
 
 hl.device({
     name = "wacom-intuos-s-pen",

a4f388df8d577691c10a3cd1ccec0c0c839bcc79
Ignacy Pękała <ignacy@pekala.eu>
2026-07-05 22:19:13 +0200

    Remove misc

--- profiles/hyprland/.config/hypr/modules/input.lua
+++ profiles/hyprland/.config/hypr/modules/input.lua
@@ -1,20 +1,19 @@
 hl.config({
     input = {
-        kb_layout    = "pl",
-        kb_variant   = "",
-        kb_model     = "",
-        kb_options   = "",
-        kb_rules     = "",
+        kb_layout = "pl",
+        kb_variant = "",
+        kb_model = "",
+        kb_options = "",
+        kb_rules = "",
 
-        -- Supposedly fixes an issue with a ghost backspace hold.
         repeat_delay = 300,
         repeat_rate = 25,
 
         follow_mouse = 2,
 
-        sensitivity  = 0, -- -1.0 - 1.0, 0 means no modification.
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
 
-        touchpad     = {
+        touchpad = {
             natural_scroll = false,
         },
     },
@@ -27,7 +26,7 @@ hl.gesture({
 })
 
 hl.device({
-    name        = "sinowealth-wired-gaming-mouse",
+    name = "sinowealth-wired-gaming-mouse",
     sensitivity = -0.5,
 })
 

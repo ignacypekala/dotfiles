6011553268a4be00a94c33e9b7b070f150afc853
Ignacy Pękała <ignacy@pekala.eu>
2026-07-05 22:18:24 +0200

    Add behaviour options of hyprland

---
+++ profiles/hyprland/.config/hypr/modules/behaviour.lua
@@ -0,0 +1,14 @@
+hl.config({
+    general = {
+        resize_on_border = false,
+        allow_tearing = false,
+        layout = "dwindle",
+    },
+    animations = {
+        enabled = true,
+    },
+    misc = {
+        force_default_wallpaper = 0,
+        disable_hyprland_logo   = true,
+    },
+})

c48a79479bcdec02bf180ca9449b4e23e8338543
Ignacy Pękała <ignacy@pekala.eu>
2026-07-07 19:06:00 +0200

    Update nvim colors.lua

--- common/.config/nvim/lua/colors.lua
+++ common/.config/nvim/lua/colors.lua
@@ -1,6 +1,17 @@
 local M = {
-    background = "#121212",
-    foreground = "#cccccc",
+    background = "{{ ui_bg }}",
+    foreground = "{{ ui_fg }}",
+    neutral = {
+        [100] = "#ffffff",
+        [200] = "#cccccc",
+        [300] = "#a6a6a6",
+        [400] = "#808080",
+        [500] = "#5e5e5e",
+        [600] = "#404040",
+        [700] = "#191919",
+        [800] = "#121212",
+        [900] = "#050505",
+    },
     ansi = {
         black = "#232323",
         red = "#d44d4f",
@@ -31,11 +42,29 @@ local M = {
        cyan = "#32424a",
        white = "#343638",
     },
-    primary = "",
-    secondary = "",
-    accent = "",
+    diff = {
+        add = {
+            line_bg = "#233726",
+            bg = "#67a376",
+            line_fg = "transparent",
+            fg = "#cccccc"
+        },
+        remove = {
+            line_bg = "#4b2526",
+            bg = "#d44d4f",
+            line_fg = "transparent",
+            fg = "#cccccc"
+        },
+        change = {
+            line_bg = "#28364d",
+            bg = "#78a7f0",
+            line_fg = "transparent",
+            fg = "#cccccc"
+        }
+    },
+    primary = "#67a376",
+    secondary = "#f4c451",
 }
-M.primary = M.ansi.green
-M.secondary = M.ansi.blue
-M.accent = M.ansi.yellow
 return M
+
+-- vim:filetype=lua

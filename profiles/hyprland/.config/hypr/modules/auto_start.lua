95978f81cef3dc174c782d24a092d75586b98fe2
Ignacy Pękała <ignacy@pekala.eu>
2026-08-27 17:36:35 +0200

    Disable quickshell autostart

--- profiles/hyprland/.config/hypr/modules/auto_start.lua
+++ profiles/hyprland/.config/hypr/modules/auto_start.lua
@@ -1,4 +1,4 @@
-local run_cmd = require("utils.run_cmd")
-hl.on("hyprland.start", function()
-    hl.exec_cmd(run_cmd("quickshell"))
-end)
+-- local run_cmd = require("utils.run_cmd")
+-- hl.on("hyprland.start", function()
+--     hl.exec_cmd(run_cmd("quickshell"))
+-- end)

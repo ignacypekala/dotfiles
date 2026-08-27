97bed57fda13a038fb7facf8a4a48636313fbd5c
Ignacy Pękała <ignacy@pekala.eu>
2026-08-27 18:02:20 +0200

    Remove default configuration.nix comments

--- profiles/nixos/configuration.nix
+++ profiles/nixos/configuration.nix
@@ -155,32 +155,8 @@
         nvidiaSettings = true;
     };
 
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
-
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
     networking.nameservers = [ "192.168.1.1" ];
     networking.networkmanager.dns = "none";
 
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "26.05"; # Did you read the comment?
+    system.stateVersion = "26.05";
 }

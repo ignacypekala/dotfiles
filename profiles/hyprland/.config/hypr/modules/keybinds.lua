bce47548879a652d56e30bee8cba05fa5cca6c35
Ignacy Pękała <ignacy@pekala.eu>
2026-09-01 18:58:49 +0200

    Prevent notification on screenshot

--- profiles/hyprland/.config/hypr/modules/keybinds.lua
+++ profiles/hyprland/.config/hypr/modules/keybinds.lua
@@ -137,7 +137,7 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
 
-hl.bind("Print", hl.dsp.exec_cmd("dms screenshot region --no-file"))
+hl.bind("Print", hl.dsp.exec_cmd("dms screenshot region --no-file --no-notify"))
 
 -- silly vim addict side effect preventio-inator
 local ctrl_w = hl.bind("CTRL + W", function()

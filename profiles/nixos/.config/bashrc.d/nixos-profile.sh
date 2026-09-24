801fc81fb77cdd908a58cc676f699b2a0aa05ad1
Ignacy Pękała <242784490+ignacypekala@users.noreply.github.com>
2026-09-24 20:11:35 +0200

    Add an upgrade alias

--- profiles/nixos/.config/bashrc.d/nixos-profile.sh
+++ profiles/nixos/.config/bashrc.d/nixos-profile.sh
@@ -1,4 +1,5 @@
 nixos_config="$HOME/configuration.nix"
 alias rebuild="sudo nixos-rebuild switch -I nixos-config=$nixos_config"
+alias upgrade="sudo nixos-rebuild switch -I nixos-config=$nixos_config --upgrade"
 
 start_ssh_agent

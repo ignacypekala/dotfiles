df2a386ee7e0d0e7ac4a16bf7c6ca5677860ca52
Ignacy Pękała <242784490+ignacypekala@users.noreply.github.com>
2026-09-24 18:29:01 +0200

    Disable confirmation when screenshotting

--- profiles/hyprland/.config/hypr/modules/keybinds.lua
+++ profiles/hyprland/.config/hypr/modules/keybinds.lua
@@ -136,7 +136,7 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
 
-hl.bind("Print", hl.dsp.exec_cmd("dms screenshot region --no-file --no-notify"))
+hl.bind("Print", hl.dsp.exec_cmd("dms screenshot region --no-file --no-confirm"))
 
 -- silly vim addict side effect preventio-inator
 local ctrl_w = hl.bind("CTRL + W", function()

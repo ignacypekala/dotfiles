a76b12a9e40f03ee02eb02bacc24c1201f1044dd
Ignacy Pękała <242784490+ignacypekala@users.noreply.github.com>
2026-09-24 20:10:59 +0200

    Set hyprland background to black

--- profiles/hyprland/.config/hypr/modules/behaviour.lua
+++ profiles/hyprland/.config/hypr/modules/behaviour.lua
@@ -13,7 +13,9 @@ hl.config({
     },
     misc = {
         force_default_wallpaper = 0,
-        disable_hyprland_logo   = true,
-        focus_on_activate = true
+        disable_hyprland_logo = true,
+        focus_on_activate = true,
+        disable_splash_rendering = true,
+        background_color = "#000000"
     },
 })
